d93179c48541b9b00b441a33e11ab791217b7bb5
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2018-11-14 21:49:28 +0100

    .

---
+++ SpringBoot/web/rest/rest-hello-world/src/main/java/mggdevit/resthelloworld/RestHelloWorldApplication.java
@@ -0,0 +1,20 @@
+package mggdevit.resthelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RestHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestHelloWorldApplication.class, args);
+	}
+
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "Hello World";
+	}
+}
