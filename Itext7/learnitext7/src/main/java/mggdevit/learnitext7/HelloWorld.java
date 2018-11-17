220286b7b45cde961606d75a07a964a9c705eaf5
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2018-11-17 20:52:01 +0100

    .

--- Itext7/learnitext7/src/main/java/mggdevit/learnitext7/HelloWorld.java
+++ Itext7/learnitext7/src/main/java/mggdevit/learnitext7/HelloWorld.java
@@ -1,13 +1,34 @@
 package mggdevit.learnitext7;
 
+import java.io.FileNotFoundException;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+//import com.itextpdf.test.annotations.WrapToTest;
+
 /**
- * Hello world!
+ * Learn Itext7 Hello world!
  *
  */
-public class HelloWorld 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+//@WrapToTest
+public class HelloWorld {
+	public static void main(String[] args) {
+		// System.out.println( "Hello World!" );
+		new HelloWorld().createPDF(".\\LearnItext7-HelloWorld.pdf");
+	}
+
+	private void createPDF(String sFileName) {
+		try {
+			PdfWriter writer = new PdfWriter(sFileName);
+			PdfDocument pdf = new PdfDocument(writer);
+			Document document = new Document(pdf);
+			document.add(new Paragraph("Hello World!"));
+			document.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
