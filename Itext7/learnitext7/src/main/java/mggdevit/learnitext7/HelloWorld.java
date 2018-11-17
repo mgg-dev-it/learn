0c12954e8a040aefbaf7f88d6258d597e7e9e227
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2018-11-17 20:33:16 +0100

    .

---
+++ Itext7/learnitext7/src/main/java/mggdevit/learnitext7/HelloWorld.java
@@ -0,0 +1,13 @@
+package mggdevit.learnitext7;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
