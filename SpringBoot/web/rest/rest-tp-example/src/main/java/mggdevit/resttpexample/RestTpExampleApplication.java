af329b8720052fb0d9862a096cab6ddd24173bbd
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2018-11-21 22:09:07 +0100

    Tutorialspoint example
    Spring Boot - Building RESTful Web Services

---
+++ SpringBoot/web/rest/rest-tp-example/src/main/java/mggdevit/resttpexample/RestTpExampleApplication.java
@@ -0,0 +1,14 @@
+package mggdevit.resttpexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTpExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTpExampleApplication.class, args);
+	}
+	
+	//https://www.tutorialspoint.com/spring_boot/spring_boot_building_restful_web_services.htm
+}
