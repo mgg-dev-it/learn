ad43c2acb12b398c74e4e9bf6fc4b202301fecc9
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2018-11-21 09:19:06 +0100

    requestmapping method

--- SpringBoot/web/rest/rest-request-mapping/src/main/java/mggdevit/restrequestmapping/Controller1.java
+++ SpringBoot/web/rest/rest-request-mapping/src/main/java/mggdevit/restrequestmapping/Controller1.java
@@ -1,14 +1,25 @@
 package mggdevit.restrequestmapping;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller1 {
 
 	@RequestMapping("/control1")
-	public String control() {
+	public String control1() {
 		return ("Controller 1");
 	}
 
+	@RequestMapping(value = "/control", method = RequestMethod.GET)
+	public String controlGET() {
+		return ("Controller GET");
+	}
+	
+	@RequestMapping(value = "/control", method = RequestMethod.POST)
+	public String controlPOST() {
+		return ("Controller POST");
+	}
+	
 }
