81ad6d44306b1e318f855281ea90ced1196f622b
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2018-11-21 21:51:47 +0100

    some mapping

--- SpringBoot/web/rest/rest-request-mapping/src/main/java/mggdevit/restrequestmapping/Controller1.java
+++ SpringBoot/web/rest/rest-request-mapping/src/main/java/mggdevit/restrequestmapping/Controller1.java
@@ -1,7 +1,9 @@
 package mggdevit.restrequestmapping;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,10 +18,26 @@ public class Controller1 {
 	public String controlGET() {
 		return ("Controller GET");
 	}
-	
+
 	@RequestMapping(value = "/control", method = RequestMethod.POST)
 	public String controlPOST() {
 		return ("Controller POST");
 	}
-	
+
+	@RequestMapping(value = "/control/{id}", method = RequestMethod.GET)
+	public String controlPathVariable(@PathVariable("id") long id) {
+		return ("Controller GET with path variable: " + id);
+	}
+
+	//the stricter / narrower mapping "wins" - see the simple @RequestMapping(value = "/control", method = RequestMethod.GET) above
+	@RequestMapping(value = "/control", params = "id", method = RequestMethod.GET)
+	public String controlRequestParam(@RequestParam("id") long id) {
+		return ("Controller GET with request param: " + id);
+	}
+
+	@RequestMapping("*")
+	public String controlFallback() {
+		return ("Fallback");
+	}
+
 }
