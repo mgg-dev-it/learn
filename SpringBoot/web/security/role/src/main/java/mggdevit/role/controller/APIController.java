54c66c5482a6c9fe52f0a03fb92682fbe66cd590
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2018-12-16 23:36:38 +0100

    SFJ II. / 7. Role

---
+++ SpringBoot/web/security/role/src/main/java/mggdevit/role/controller/APIController.java
@@ -0,0 +1,26 @@
+package mggdevit.role.controller;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class APIController {
+
+	@RequestMapping("/")
+	public String index() {
+		return("Main Page");
+	}
+	
+	@Secured("ROLE_USER")
+	@RequestMapping("/userpage")
+	public String userPage() {
+		return("User Page");
+	}
+	
+	@Secured("ROLE_ADMIN")
+	@RequestMapping("/adminpage")
+	public String adminPage() {
+		return("Admin Page");
+	}
+}
