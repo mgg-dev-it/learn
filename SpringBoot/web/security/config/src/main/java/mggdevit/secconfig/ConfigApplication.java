01ff58741f9466437b8973d6a161f27fc963468b
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2019-01-01 17:19:15 +0100

    .

---
+++ SpringBoot/web/security/config/src/main/java/mggdevit/secconfig/ConfigApplication.java
@@ -0,0 +1,23 @@
+package mggdevit.secconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * SanFranciscobolJottem
+ * 
+ * Spring Boot II. 08. Spring Security - Config
+ * 
+ * {noop} needed before the plain passwords
+ * 
+ */
+
+@SpringBootApplication
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+}
+
