5f79d791092ca2e0d94e473ffbd9f286c93e1623
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2019-01-04 00:05:42 +0100

    .

--- SpringBoot/web/security/sec-db-auth/src/main/java/mggdevit/secdbauth/SecDbAuthApplication.java
+++ SpringBoot/web/security/sec-db-auth/src/main/java/mggdevit/secdbauth/SecDbAuthApplication.java
@@ -3,6 +3,13 @@ package mggdevit.secdbauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * SanFranciscobolJottem
+ * 
+ * Spring Boot II. 17. Spring Security - DB Authentication
+ * 
+ */
+
 @SpringBootApplication
 public class SecDbAuthApplication {
 
