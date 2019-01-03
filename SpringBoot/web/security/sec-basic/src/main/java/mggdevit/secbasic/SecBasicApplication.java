307f3f1676a8ac2977a6c255c202ba9894d7e423
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2019-01-03 23:44:28 +0100

    .

---
+++ SpringBoot/web/security/sec-basic/src/main/java/mggdevit/secbasic/SecBasicApplication.java
@@ -0,0 +1,21 @@
+package mggdevit.secbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * SanFranciscobolJottem
+ * 
+ * Spring Boot II. 06. Spring Security - Basic authentication
+ * 
+ */
+
+@SpringBootApplication
+public class SecBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecBasicApplication.class, args);
+	}
+
+}
+
---
+++ SpringBoot/web/security/sec-db-auth/src/main/java/mggdevit/secdbauth/SecDbAuthApplication.java
@@ -0,0 +1,14 @@
+package mggdevit.secdbauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecDbAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecDbAuthApplication.class, args);
+	}
+
+}
+
