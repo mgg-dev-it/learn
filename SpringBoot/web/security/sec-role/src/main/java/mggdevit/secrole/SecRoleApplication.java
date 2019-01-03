9eba96cf4c9e4f71bdca7a703fa7e7efac04b7f5
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2019-01-03 23:52:12 +0100

    .

---
+++ SpringBoot/web/security/sec-role/src/main/java/mggdevit/secrole/SecRoleApplication.java
@@ -0,0 +1,21 @@
+package mggdevit.secrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * SanFranciscobolJottem
+ * 
+ * Spring Boot II. 07. Spring Security - Role
+ * 
+ */
+
+@SpringBootApplication
+public class SecRoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecRoleApplication.class, args);
+	}
+
+}
+
---
+++ SpringBoot/web/security/sec-role/src/main/java/mggdevit/secrole/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package mggdevit.secrole.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableGlobalMethodSecurity(securedEnabled = true)
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+}
