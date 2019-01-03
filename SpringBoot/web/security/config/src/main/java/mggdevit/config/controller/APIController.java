80ab26d4c8c37c17e371024029e14d0718c66c64
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2019-01-03 10:20:48 +0100

    .

--- SpringBoot/web/security/config/src/main/java/mggdevit/config/controller/APIController.java
+++ SpringBoot/web/security/config/src/main/java/mggdevit/config/controller/APIController.java
@@ -1,6 +1,5 @@
 package mggdevit.config.controller;
 
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
