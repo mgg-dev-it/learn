4be736c18fc31b9de0edcb124c3bcae4913bb356
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2019-01-04 22:26:53 +0100

    .

--- SpringBoot/web/security/sec-db-auth/src/main/java/mggdevit/secdbauth/config/SecurityConfig.java
+++ SpringBoot/web/security/sec-db-auth/src/main/java/mggdevit/secdbauth/config/SecurityConfig.java
@@ -7,34 +7,48 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+//	@Autowired
+//	public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
+//		auth
+//		.inMemoryAuthentication()
+//		.withUser("user").password("{noop}1234").roles("USER")
+//		.and()
+//		.withUser("admin").password("{noop}1234").roles("USER","ADMIN");
+//	}
+
 	@Autowired
-	public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
-
-		auth
-		.inMemoryAuthentication()
-		.withUser("user").password("{noop}1234").roles("USER")
-		.and()
-		.withUser("admin").password("{noop}1234").roles("USER","ADMIN");
+	private UserDetailsService userService;
+	
+	@Autowired
+	public void configureAuth(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userService);
+		//@todo auth.jdbcAuthentication(); //https://www.boraji.com/spring-security-5-jdbc-based-authentication-example
 	}
-
+	
 	@Override
 	protected void configure(HttpSecurity httpSec) throws Exception {
 		httpSec
 		.authorizeRequests()
 		 //.antMatchers(HttpMethod.GET, "/").permitAll()
 		 .antMatchers("/").permitAll()
-		 .antMatchers("/db/**").permitAll()
+		 //.antMatchers("/db/**").permitAll() //only for h2 console
 		 .antMatchers("/userpage").hasRole("USER")
 		 .antMatchers("/adminpage/**").hasRole("ADMIN")
+		 .anyRequest().authenticated()
 //		 .antMatchers("/delete").hasRole("ADMIN")
 //		 .antMatchers("/admin/**").hasRole("ADMIN")
-		 .and().formLogin().permitAll();
+		 .and()
+		.formLogin().permitAll();
+		
+		//httpSec.csrf().disable(); //only for h2 console
+		//httpSec.headers().frameOptions().sameOrigin(); //only for h2 console
 	}
 
 }
