0903cdf453c2ff9e6ac6d719c2f74af093b06eb7
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2019-01-06 20:40:40 +0100

    .

---
+++ SpringBoot/web/try/try-alpha/src/main/java/mggdevit/tryalpha/TryAlphaApplication.java
@@ -0,0 +1,14 @@
+package mggdevit.tryalpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryAlphaApplication.class, args);
+	}
+
+}
+
---
+++ SpringBoot/web/try/try-alpha/src/main/java/mggdevit/tryalpha/controller/HomeController.java
@@ -0,0 +1,19 @@
+package mggdevit.tryalpha.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+//	@RequestMapping("/")
+//	public String index() {
+//		return ("index");
+//	}
+
+	@RequestMapping("/list")
+	public String list() {
+		return ("list");
+	}
+
+}
