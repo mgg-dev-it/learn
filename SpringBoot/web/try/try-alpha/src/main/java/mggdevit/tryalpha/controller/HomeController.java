e1bb032e29c510098b7f2e39eec3bd830a5c60b8
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2019-01-08 00:01:45 +0100

    .

--- SpringBoot/web/try/try-alpha/src/main/java/mggdevit/tryalpha/controller/HomeController.java
+++ SpringBoot/web/try/try-alpha/src/main/java/mggdevit/tryalpha/controller/HomeController.java
@@ -1,19 +1,48 @@
 package mggdevit.tryalpha.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import mggdevit.tryalpha.model.Person;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
-	public String index() {
-		return ("other");
+	private static List<Person> persons = new ArrayList<Person>();
+	static {
+		persons.add(new Person("John", "Smith"));
+		persons.add(new Person("George", "Tailor"));
+	}
+	
+//	@RequestMapping("/")
+//	public String index() {
+//		return ("index");
+//	}
+
+	@RequestMapping("/tmp")
+	public String tmp() {
+		return ("tmp");
 	}
 
-	@RequestMapping("/list")
-	public String list() {
-		return ("list");
+	@RequestMapping("/view")
+	public String view(Map<String, Object> model) {
+		model.put("message", "foo");
+		return ("view");
 	}
 
+	@RequestMapping("/personlist")
+	public String personlist(Model model) {
+		model.addAttribute("persons", persons);
+		return ("personlist");
+	}
+	
+	// https://www.mkyong.com/spring-boot/spring-boot-hello-world-example-thymeleaf/
+	// https://spring.io/guides/gs/serving-web-content/
+	// https://www.baeldung.com/spring-boot-crud-thymeleaf
+	// https://o7planning.org/en/11545/spring-boot-and-thymeleaf-tutorial
 }
