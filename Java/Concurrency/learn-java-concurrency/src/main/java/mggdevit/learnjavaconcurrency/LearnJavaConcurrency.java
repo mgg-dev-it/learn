9d476f6f6b0a88503c64f30321f30da4b1c90759
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2019-02-04 10:18:21 +0100

    .

---
+++ Java/Concurrency/learn-java-concurrency/src/main/java/mggdevit/learnjavaconcurrency/LearnJavaConcurrency.java
@@ -0,0 +1,16 @@
+package mggdevit.learnjavaconcurrency;
+
+public class LearnJavaConcurrency {
+
+	public static void main(String[] args) {
+		LearnJavaConcurrency.learn();
+	}
+
+	public static void learn() {
+		System.out.println("");
+		System.out.println("LEARNING CONCURRENCY");
+		System.out.println("");
+		LearnThread.learn();
+		System.out.println("");
+	}
+}
---
+++ Java/Concurrency/learn-java-concurrency/src/main/java/mggdevit/learnjavaconcurrency/LearnThread.java
@@ -0,0 +1,12 @@
+package mggdevit.learnjavaconcurrency;
+
+public class LearnThread {
+	public LearnThread() {
+	}
+
+	public static void learn() {
+		System.out.println("");
+		System.out.println("Learning Threads");
+		System.out.println("");
+	}
+}
