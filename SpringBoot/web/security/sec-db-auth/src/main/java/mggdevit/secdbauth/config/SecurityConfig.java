83c731ba041265fb2638493672937a5dd06c692b
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2019-02-26 08:04:39 +0100

    fix imports

--- SpringBoot/web/security/sec-db-auth/src/main/java/mggdevit/secdbauth/config/SecurityConfig.java
+++ SpringBoot/web/security/sec-db-auth/src/main/java/mggdevit/secdbauth/config/SecurityConfig.java
@@ -2,13 +2,11 @@ package mggdevit.secdbauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
