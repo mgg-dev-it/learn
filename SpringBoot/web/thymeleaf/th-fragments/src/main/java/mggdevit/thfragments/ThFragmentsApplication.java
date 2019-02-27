c1bead6b215a81e50dabb5fc2470920320f8e783
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2019-02-28 00:24:38 +0100

    header-footer

---
+++ SpringBoot/web/thymeleaf/th-fragments/src/main/java/mggdevit/thfragments/ThFragmentsApplication.java
@@ -0,0 +1,13 @@
+package mggdevit.thfragments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThFragmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThFragmentsApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/web/thymeleaf/th-fragments/src/test/java/mggdevit/thfragments/ThFragmentsApplicationTests.java
@@ -0,0 +1,16 @@
+package mggdevit.thfragments;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThFragmentsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
