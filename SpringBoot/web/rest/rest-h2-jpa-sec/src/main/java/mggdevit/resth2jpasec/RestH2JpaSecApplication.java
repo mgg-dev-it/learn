4d94cfad5251e0fa074cd2a48eb9bd6c786ca698
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2019-03-10 17:50:16 +0100

    .

--- SpringBoot/web/rest/rest-h2-jpa-sec/src/main/java/mggdevit/resth2jpasec/RestH2JpaSecApplication.java
+++ SpringBoot/web/rest/rest-h2-jpa-sec/src/main/java/mggdevit/resth2jpasec/RestH2JpaSecApplication.java
@@ -7,9 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import mggdevit.resth2jpasec.domain.Car;
-import mggdevit.resth2jpasec.domain.Owner;
 import mggdevit.resth2jpasec.domain.CarRepository;
+import mggdevit.resth2jpasec.domain.Owner;
 import mggdevit.resth2jpasec.domain.OwnerRepository;
+import mggdevit.resth2jpasec.domain.User;
+import mggdevit.resth2jpasec.domain.UserRepository;
 
 /*
  * Based on the 3rd and 4th chapter of the book "Hands-On Full Stack Development with Spring Boot 2.0 and React" by Juha Hinkula
@@ -23,6 +25,9 @@ public class RestH2JpaSecApplication {
 
 	@Autowired
 	private OwnerRepository orepository;
+	
+	@Autowired
+	private UserRepository urepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestH2JpaSecApplication.class, args);
@@ -39,6 +44,8 @@ public class RestH2JpaSecApplication {
 			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
 			repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
 			repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
-		};
+			
+			urepository.save(new User("user", "$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi", "USER"));
+			urepository.save(new User("admin", "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG", "ADMIN"));		};
 	}
 }
