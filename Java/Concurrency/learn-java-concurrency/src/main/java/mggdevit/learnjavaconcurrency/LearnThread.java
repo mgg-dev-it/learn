058ed24db64d65b9e3fc3a200b9d179b0ff3eea1
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2019-05-20 14:19:08 +0200

    .

--- Java/Concurrency/learn-java-concurrency/src/main/java/mggdevit/learnjavaconcurrency/LearnThread.java
+++ Java/Concurrency/learn-java-concurrency/src/main/java/mggdevit/learnjavaconcurrency/LearnThread.java
@@ -1,15 +1,28 @@
 package mggdevit.learnjavaconcurrency;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class LearnThread {
 
 	private final static int CYCLE = 1000;
 	private static int iCount = 0;
+	private static AtomicInteger ai;
 
 	class A implements Runnable {
 
 		public void run() {
 			for (int i = 0; i < CYCLE; i++) {
 				++iCount;
+			}
+
+		}
+	}
+
+	class B implements Runnable {
+
+		public void run() {
+			for (int i = 0; i < CYCLE; i++) {
+				ai.incrementAndGet();
 			}
 
 		}
@@ -24,6 +37,7 @@ public class LearnThread {
 		System.out.println("");
 		System.out.println("The wrong increment:");
 		System.out.println("");
+
 		A a = new A();
 		Thread t1 = new Thread(a);
 		Thread t2 = new Thread(a);
@@ -33,9 +47,25 @@ public class LearnThread {
 			t1.join();
 			t2.join();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		System.out.println("iCount = " + iCount + " (it should be " + 2 * CYCLE + " !)");
+
+		System.out.println("");
+
+		ai = new AtomicInteger(0);
+		B b = new B();
+		Thread t11 = new Thread(b);
+		Thread t12 = new Thread(b);
+		t11.start();
+		t12.start();
+		try {
+			t11.join();
+			t12.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("ai.intValue() = " + ai.intValue() + " (it must be " + 2 * CYCLE + " :)");
+
 	}
 }
