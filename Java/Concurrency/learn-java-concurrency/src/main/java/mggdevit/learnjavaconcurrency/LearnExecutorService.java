f84d972921e51abefff84dbd535b928a158d88e9
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2019-05-22 23:41:33 +0200

    .

--- Java/Concurrency/learn-java-concurrency/src/main/java/mggdevit/learnjavaconcurrency/LearnExecutorService.java
+++ Java/Concurrency/learn-java-concurrency/src/main/java/mggdevit/learnjavaconcurrency/LearnExecutorService.java
@@ -18,7 +18,11 @@ public class LearnExecutorService {
 		System.out.println("");
 		System.out.println("Learning ExecutorService");
 		System.out.println("");
+		e1();
+		e2();
+	}
 
+	private void e1() {
 		ExecutorService executorService = Executors.newFixedThreadPool(5);
 
 		Set<Callable<String>> callables = new HashSet<Callable<String>>();
@@ -33,7 +37,38 @@ public class LearnExecutorService {
 		callables.add(createCallable(8));
 		callables.add(createCallable(9));
 		callables.add(createCallable(10));
-		
+
+		List<Future<String>> futures;
+		try {
+			futures = executorService.invokeAll(callables);
+			for (Future<String> future : futures) {
+				System.out.println("future.get = " + future.get());
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+
+		executorService.shutdown();
+	}
+
+	private void e2() {
+		ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+		Set<Callable<String>> callables = new HashSet<Callable<String>>();
+
+		callables.add(createCallable(101));
+		callables.add(createCallable(102));
+		callables.add(createCallable(103));
+		callables.add(createCallable(104));
+		callables.add(createCallable(105));
+		callables.add(createCallable(106));
+		callables.add(createCallable(107));
+		callables.add(createCallable(108));
+		callables.add(createCallable(109));
+		callables.add(createCallable(110));
+
 		List<Future<String>> futures;
 		try {
 			futures = executorService.invokeAll(callables);
