48889dacc11d8c058e0482ccada42e9f77bccf0e
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2020-01-13 13:15:13 +0100

    .

--- NodeJS/test/test001.js
+++ NodeJS/test/test001.js
@@ -1,10 +1,19 @@
+//console.log(process.env);
 //console.log(process.env.DEBUG);
-process.env.DEBUG = true;
+//process.env.DEBUG = true;
 //console.log(process.env.DEBUG);
 
 //console.log(`process.platform = ${process.platform}`);
 
 //console.log(process.env);
+
+
+
+const db_test_dbms = process.env.db_test_dbms;
+const db_test_driver = process.env.db_test_driver;
+const db_test_server = process.env.db_test_server;
+const db_test_username = process.env.db_test_username;
+const db_test_password = process.env.db_test_password;
 
 const magjslib = require('mag-js-lib');
 
@@ -19,7 +28,7 @@ console.log("begin");
 
 auth.login("admin", "admin");
 
-db.init("mssql", "tedious", "server", "user", "pass");
-db2.init("mssql", "ms", "server", "user", "pass");
+db.init(db_test_dbms, db_test_driver, db_test_server, db_test_username, db_test_password);
+//db2.init("mssql", "ms", "server", "user", "pass");
 
 console.log("end");
