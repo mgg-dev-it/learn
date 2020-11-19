714803a2600f84078abd6783be9f5c7fa1761dce
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2020-11-19 21:36:19 +0100

    .

--- NodeJS/test/testletlab.js
+++ NodeJS/test/testletlab.js
@@ -5,12 +5,4 @@ const magjslib = require('mag-js-lib');
 
 const letlab = magjslib.letlab;
 
-<<<<<<< HEAD
-//letlab.letterLabyrinth();
-
-//letlab.consoleTest();
-
-letlab.consoleHtmlTest();
-=======
 letlab.letterLabyrinth();
->>>>>>> cd661fc7da5640819f07ed0c4ec365b9fc1866f9
