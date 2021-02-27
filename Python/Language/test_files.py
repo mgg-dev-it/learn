3c86a38a2e9d82051cbfa7432b5906367102c6e6
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2021-02-27 22:51:16 +0100

    .

--- Python/Language/test_files.py
+++ Python/Language/test_files.py
@@ -1,9 +1,11 @@
 import os
 
+cwd = os.getcwd()
+# print ('Current working directory is:', cwd)
+print (f'Current working directory is: \'{cwd}\'')
+
 dircontent = os.listdir()
-
 print(dircontent)
-
 for d in dircontent:
     print(d)
-    
+
