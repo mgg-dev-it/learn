d98809c2568e01c3883ca6407e2cd1883f7f06f5
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2021-02-28 18:59:30 +0100

    .

--- Python/Language/test_json.py
+++ Python/Language/test_json.py
@@ -4,21 +4,22 @@
 
 import json
 
-print (json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}]))
+print(json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}]))
 
-print (json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}], sort_keys=True, indent=4))
+print(json.dumps(
+    ['foo', {'bar': ('baz', None, 1.0, 2)}], sort_keys=True, indent=4))
 
 print(json.dumps({'4': 5, '6': 7}, sort_keys=True, indent=4))
 
 o = json.loads('["foo", {"bar":["baz", null, 1.0, 2]}]')
 
-print (type (o))
+print(type(o))
 
-print (o)
+print(o)
 
-print (json.loads ('{    "4": 5,    "6": 7}'))
+print(json.loads('{    "4": 5,    "6": 7}'))
 
-#---
+# ---
 
 data = {
     "president": {
@@ -26,10 +27,10 @@ data = {
         "species": "Betelgeusian"
     }
 }
-print (json.dumps (data))
-print (json.dumps (data, indent=4))
+print(json.dumps(data))
+print(json.dumps(data, indent=4))
 
-#---
+# ---
 
 json_string = """
 {
@@ -46,4 +47,36 @@ json_string = """
 }
 """
 data = json.loads(json_string)
-print (data)
+print(data)
+
+# ---
+
+menu = {
+    "breakfast": {
+        "hours": "7-11",
+        "items": {
+            "breakfast burritos": "$6.00",
+            "pancakes": "$4.00"
+        }
+    },
+    "lunch": {
+        "hours": "11-3",
+        "items": {
+            "hamburger": "$5.00"
+        }
+    },
+    "dinner": {
+        "hours": "3-10",
+        "items": {
+            "spaghetti": "$8.00"
+        }
+    }
+}
+print (type(menu))
+print (menu)
+menu_json = json.dumps(menu)
+#print(menu_json)
+menu2 = json.loads(menu_json)
+print (type(menu2))
+print (menu2)
+
