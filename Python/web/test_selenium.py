e6661eb7b8bb8d2c49c56d1bcb6c7ea6535be47e
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2021-02-28 20:03:32 +0100

    .

--- Python/web/test_selenium.py
+++ Python/web/test_selenium.py
@@ -2,14 +2,22 @@
 # https://www.javatpoint.com/selenium-python
 # https://www.guru99.com/selenium-python.html
 # https://www.geeksforgeeks.org/selenium-python-tutorial/
+# https://www.browserstack.com/guide/python-selenium-to-run-web-automation-test
+# https://www.selenium.dev/
+# https://chromedriver.chromium.org/capabilities
+# https://www.programcreek.com/python/example/100025/selenium.webdriver.ChromeOptions
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.chrome.options import Options
 import time  
 from selenium.webdriver.common.keys import Keys  
 print("sample test case started")
 
-options = Options()
+caps = webdriver.DesiredCapabilities.CHROME.copy()
+print (caps)
+
+#options = Options()
+options = webdriver.ChromeOptions()
 #options.add_argument("--disable-extensions")
 #options.set_headless(headless=True)
 #options.headless = True
@@ -30,12 +38,12 @@ driver.get("https://www.google.com/")
 #identify the Google search text box and enter the value  
 driver.find_element_by_name("q").send_keys("javatpoint")  
 
-time.sleep(3)  
+time.sleep(2)  
 
 #click on the Google search button  
 driver.find_element_by_name("btnK").send_keys(Keys.ENTER)  
 
-time.sleep(3)  
+time.sleep(2)  
 
 #close the browser  
 driver.close()  
