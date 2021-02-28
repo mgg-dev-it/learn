9359354f710352489999330fe20e614e45f6f646
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2021-02-28 17:02:06 +0100

    .

--- Python/Language/test_files.py
+++ Python/Language/test_files.py
@@ -1,3 +1,6 @@
+# https://docs.python.org/3/library/fileinput.html
+# https://docs.python.org/3/library/os.html
+
 import os
 
 cwd = os.getcwd()
--- Python/Language/test_os.py
+++ Python/Language/test_os.py
@@ -1,4 +1,6 @@
 # https://docs.python.org/3/library/os.html
+# https://docs.python.org/3/library/os.path.html
+
 import os
 
 cwd = os.getcwd()
@@ -12,4 +14,17 @@ print(type(env))
 for e in env:
     #clearprint (e, ' = ', os.environ[e])
     print (f'{e} = { os.environ[e]}')
+
+print (os.path.expanduser("~"))
+print (os.path.expanduser("~abc"))
+
+print (f'os.sep = {os.sep}')
+print (f'os.altsep = {os.altsep}')
+print (f'os.extsep = {os.extsep}')
+print (f'os.pathsep = {os.pathsep}')
+print (f'os.defpath = {os.defpath}')
+print (f'os.devnull = {os.devnull}')
+
+print (os.path.join("abc", "def", "ghi"))
+print (os.path.join("c:", os.sep, "abc", "def", "ghi"))
 
