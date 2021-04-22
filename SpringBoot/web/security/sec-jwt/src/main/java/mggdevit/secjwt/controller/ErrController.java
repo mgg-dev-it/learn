24e09d19a173074f426a96f9e1d62ab7cd772c0b
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2021-04-22 20:28:11 +0200

    .

---
+++ SpringBoot/web/security/sec-jwt/src/main/java/mggdevit/secjwt/controller/ErrController.java
@@ -0,0 +1,59 @@
+package mggdevit.secjwt.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
+
+@RestController
+public class ErrController implements ErrorController {
+
+	private static final String ERR_PATH = "/error";
+
+	private ErrorAttributes errorAttributes;
+
+	@Autowired
+	public void setErrorAttributes(ErrorAttributes errorAttributes) {
+		this.errorAttributes = errorAttributes;
+
+	}
+
+	@RequestMapping(ERR_PATH)
+	public String error(Model model, HttpServletRequest request) {
+		// RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+		// Map<String, Object> errorMap =
+		// this.errorAttributes.getErrorAttributes(requestAttributes, true);
+		ServletWebRequest servletWebRequest = new ServletWebRequest(request);
+		Map<String, Object> errorAttributes = this.errorAttributes.getErrorAttributes(servletWebRequest, true);
+
+		model.addAttribute("timestamp", errorAttributes.get("timestamp"));
+		model.addAttribute("status", errorAttributes.get("status"));
+		model.addAttribute("error", errorAttributes.get("error"));
+		model.addAttribute("message", errorAttributes.get("message"));
+		model.addAttribute("trace", errorAttributes.get("trace"));
+		model.addAttribute("path", errorAttributes.get("path"));
+
+		if (errorAttributes.get("status").toString().equalsIgnoreCase("401")) {
+			return ("error401page");
+		}
+		
+		if (errorAttributes.get("status").toString().equalsIgnoreCase("404")) {
+			return ("error404page");
+		}
+		
+		return ("errorpage");
+	}
+
+	@Override
+	public String getErrorPath() {
+		return (ERR_PATH);
+	}
+
+}
--- SpringBoot/web/security/sec-jwt/src/main/java/mggdevit/secjwt/security/JwtUtils.java
+++ SpringBoot/web/security/sec-jwt/src/main/java/mggdevit/secjwt/security/JwtUtils.java
@@ -1,6 +1,5 @@
 package mggdevit.secjwt.security;
 
-//import java.security.SignatureException;
 import java.util.Date;
 
 import org.slf4j.Logger;
@@ -9,7 +8,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import mggdevit.secjwt.service.UserDetailsImpl;
 
 @Component
@@ -42,8 +46,8 @@ public class JwtUtils {
 		try {
 			Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
 			return true;
-//		} catch (SignatureException e) {
-//			logger.error("Invalid JWT signature: {}", e.getMessage());
+		} catch (SignatureException e) {
+			logger.error("Invalid JWT signature: {}", e.getMessage());
 		} catch (MalformedJwtException e) {
 			logger.error("Invalid JWT token: {}", e.getMessage());
 		} catch (ExpiredJwtException e) {
